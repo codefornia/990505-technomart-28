bb0f83fe3f04feaf02afa0f18b50700c01b97277
codefornia <sblrkina@gmail.com>
2020-07-10 14:30:56 +0300

    Заершаем строить сетку

--- js/script.js
+++ js/script.js
@@ -1,19 +1,54 @@
-<script type="application/javascript">
-  let tabsButtons = document.getElementsByClassName('tabs-button');
-for (let i = 0; i < tabsButtons.length; i++) {
-  tabsButtons[i].addEventListener('click', () => {
-    let activeTabsButtons = document.getElementsByClassName('tabs-button-active');
-    for (let j = 0; j < activeTabsButtons.length; j++) {
-      activeTabsButtons[j].classList.remove('tabs-button-active');
-    }
-    tabsButtons[i].classList.add('tabs-button-active');
-    let id = tabsButtons[i].dataset.id;
+var orderLink = document.querySelector(".button-about-contacts");
+var orderPopup = document.querySelector(".popup-order");
+var orderClose = orderPopup.querySelector(".popup-order-close");
+var orderForm = orderPopup.querySelector(".order-form");
+var userNameInput = orderPopup.querySelector(".user-name-input");
+var userEmailInput = orderPopup.querySelector(".user-email-input");
+var userCommentTextarea = orderPopup.querySelector(".user-comment-textarea");
 
-    let activeContent = document.getElementsByClassName('content-active');
-    for (let j = 0; j < activeContent.length; j++) {
-      activeContent[j].classList.remove('content-active');
-    }
-    document.getElementById(id).classList.add('content-active');
-  }, false);
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
 }
-</script>
+
+orderLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  orderPopup.classList.add("modal-show");
+  userNameInput.focus();
+});
+
+if (storage) {
+  userNameInput.value = storage;
+  userEmailInput.focus();
+} else {
+  orderPopup.focus();
+}
+orderClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  orderPopup.classList.remove("modal-show");
+  orderPopup.classList.remove("modal-error");
+});
+
+orderForm.addEventListener("submit", function (evt) {
+  if (!userNameInput.value || !userEmailInput.value || !userCommentTextarea.value) {
+  evt.preventDefault();
+  orderPopup.classList.add("modal-error");
+}else {
+    localStorage.setItem("login", userNameInput.value);
+    localStorage.setItem("email", userEmailInput.value);
+  });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (orderPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      orderPopup.classList.remove("modal-show");
+      orderPopup.classList.remove("modal-error");
+      orderPopup.classList.remove("modal-error");
+    }
+  }
+});
